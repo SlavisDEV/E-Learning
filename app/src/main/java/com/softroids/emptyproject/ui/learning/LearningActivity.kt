91ce62f32418d56e63851526dbd5c5d81d0d2b29
Sławomir Przybylski <slavis.dev@gmail.com>
2020-01-04 17:05:29 +0100

    init

---
+++ app/src/main/java/com/softroids/emptyproject/ui/learning/LearningActivity.kt
@@ -0,0 +1,13 @@
+package com.softroids.emptyproject.ui.learning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.softroids.emptyproject.R
+
+class LearningActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_learning)
+    }
+}
